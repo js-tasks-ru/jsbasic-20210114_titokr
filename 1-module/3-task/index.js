d246e94e063e9f2991245bfa29730edd919c1d08
Роман Т <titokr@gmail.com>
2021-01-18 23:14:55 +0200

    Решения задач "Занятие 1"

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,4 +5,17 @@
  */
 function ucFirst(str) {
   // ваш код...
+
+  // способ 1
+  //return (str.length > 0) ? str[0].toUpperCase() + str.substr(1) : '';
+
+  // способ 2
+  let resultStr = '';
+  if (str.length > 0) {
+    resultStr = str[0].toUpperCase();
+    for (var i = 1; i < str.length; i++) {
+      resultStr = resultStr + str[i];
+    }
+  }
+  return resultStr;
 }
