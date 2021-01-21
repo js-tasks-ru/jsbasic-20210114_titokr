136909b37f74bebfc545b02844e0610c88fb83b2
Роман Т <titokr@gmail.com>
2021-01-21 22:25:18 +0200

    Решения задач по уроку 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -5,4 +5,11 @@
  */
 function sumSalary(salaries) {
   // ваш код...
+  let sumSalaries = 0;
+  for (let key in salaries) {
+    if (typeof(salaries[key]) === 'number' && isFinite(salaries[key]) && !isNaN(salaries[key])) {
+      sumSalaries += salaries[key];
+    }
+  }
+  return sumSalaries;
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
   // ваш код
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+    return this; // возвращаем this чтобы можно было написать calculator.read(4, 6).sum();
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
