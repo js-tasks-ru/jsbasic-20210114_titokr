671524d9f0815e0ea4209aaba81708df9e725231
Роман Т <titokr@gmail.com>
2021-01-31 21:36:05 +0200

    Решения задач по уроку 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -5,4 +5,9 @@
  */
 function makeFriendsList(friends) {
   // ваш код...
+  let listFriends = document.createElement('ul');
+  for (let friend of friends) {
+    listFriends.innerHTML += `<li>${friend.firstName} ${friend.lastName}</li>`;
+  }
+  return listFriends;
 }
