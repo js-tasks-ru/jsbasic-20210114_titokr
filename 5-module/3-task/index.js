43bb9342be7b847d4e4109a9755a7a1028fbb965
Роман Т <titokr@gmail.com>
2021-02-05 22:06:59 +0200

    Решение задач по уроку 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,51 @@
 function initCarousel() {
   // ваш код...
+  let slider = document.querySelector('.carousel'),
+      inner = slider.querySelector('.carousel__inner'),
+      btnPrev = slider.querySelector('.carousel__arrow_left'),
+      btnNext = slider.querySelector('.carousel__arrow_right'),
+      widthSlide = inner.children[0].offsetWidth,
+      currentNumSlider = 0,
+      countSlides = inner.children.length;
+
+  function setSlide() {
+    inner.style.transform = 'translateX(-' + currentNumSlider * widthSlide + 'px)';
+  }
+
+  btnPrev.style.display = 'none';
+  for (let elem of inner.children) {  // чтобы при ширине окна меньше ширины контейнера - убрать баг "выпирания" картинки за пределы слайда
+    elem.style.overflow = 'hidden';
+  }
+  if (countSlides <= 1) {
+    btnNext.style.display = 'none';
+  }
+
+  window.addEventListener('resize', () => {
+    widthSlide = inner.children[0].offsetWidth;
+    setSlide();
+  });
+
+  btnNext.addEventListener('click', () => {
+    currentNumSlider++;
+    if (currentNumSlider < countSlides - 1) {      
+      btnPrev.style.display = '';
+      setSlide();
+    } else {
+      btnPrev.style.display = '';
+      btnNext.style.display = 'none';
+      setSlide();
+    }
+  });
+
+  btnPrev.addEventListener('click', () => {
+    currentNumSlider--;
+    if (currentNumSlider > 0) {
+      btnNext.style.display = '';
+      setSlide();
+    } else {
+      btnNext.style.display = '';
+      btnPrev.style.display = 'none';
+      setSlide();
+    }
+  });
 }
